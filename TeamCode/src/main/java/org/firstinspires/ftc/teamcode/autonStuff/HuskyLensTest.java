d7a5e3e6e9ac3daf0ef848b7897f4f8b3ff316e0
pwhit26 <163192415+pwhit26@users.noreply.github.com>
2024-06-11 09:25:33 -0400

    first commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonStuff/HuskyLensTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonStuff/HuskyLensTest.java
@@ -30,9 +30,9 @@ public class HuskyLensTest extends LinearOpMode {
     public static double xPosWhite = 0, yPosWhite = 20, headingWhite = 90;
     public static double xPosRed = 0, yPosRed = -20, headingRed = 90;
     public static double maxVel = 20;
+
     @Override
     public void runOpMode() throws InterruptedException {
-        Telemetry telemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());
         ArrayList<PosesAndActions> pointToGoTo = new ArrayList<>();
         Pose2d startPos = new Pose2d(xPosStart, yPosStart, Math.toRadians(headingStart));
         Pose2d blue1Pos = new Pose2d(xPosBlue1, yPosBlue1, Math.toRadians(headingBlue1));
@@ -42,96 +42,65 @@ public class HuskyLensTest extends LinearOpMode {
         Pose2d redPos = new Pose2d(xPosRed, yPosRed, Math.toRadians(headingRed));
         bot.init();
         waitForStart();
-        while (opModeInInit()){
+        Telemetry telemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());
+        while (opModeIsActive()) {
+            int largestArea = 0;
+            int position = 0;
             HuskyLens.Block[] blocks = bot.huskyLens.blocks();
             telemetry.addData("Block count", blocks.length);
             for (int i = 0; i < blocks.length; i++) {
                 telemetry.addData("Block", blocks[i].toString());
             }
-            //blue = 1 white = 2 red = 3
-            for (int i = 0; i < blocks.length; i++)
-            {
-
-                if (blocks[i].id==3)
-                {
-                    for (int j = 0; j < blocks.length; j++)
-                    {
-                        if (blocks[i].id==1)
-                        {
-                            pointToGoTo.add(new PosesAndActions(startPos, ""));
-                            pointToGoTo.add(new PosesAndActions(blue1Pos, ""));
-                            pointToGoTo.add(new PosesAndActions(blue2Pos, ""));
-                            pointToGoTo.add(new PosesAndActions(blue3Pos, ""));
-                            follower.init(pointToGoTo, false, true);
-                            waitForStart();
-                            follower.goToPoints(true, maxVel);
-                            pointToGoTo.clear();
-                            pointToGoTo.add(new PosesAndActions(startPos, ""));
-                            follower.reinit(pointToGoTo);
-                            follower.goToPoints(true, maxVel);
-                            break;
-                        }
-                        else if (blocks[i].id==2)
-                        {
-                            pointToGoTo.add(new PosesAndActions(startPos, ""));
-                            pointToGoTo.add(new PosesAndActions(whitePos, ""));
-                            follower.init(pointToGoTo, false, true);
-                            waitForStart();
-                            follower.goToPoints(true, maxVel);
-                            pointToGoTo.clear();
-                            pointToGoTo.add(new PosesAndActions(startPos, ""));
-                            follower.reinit(pointToGoTo);
-                            follower.goToPoints(true, maxVel);
-                            break;
-                        }
-                        else
-                        {
-                            pointToGoTo.add(new PosesAndActions(startPos, ""));
-                            pointToGoTo.add(new PosesAndActions(redPos, ""));
-                            follower.init(pointToGoTo, false, true);
-                            waitForStart();
-                            follower.goToPoints(true, maxVel);
-                            pointToGoTo.clear();
-                            pointToGoTo.add(new PosesAndActions(startPos, ""));
-                            follower.reinit(pointToGoTo);
-                            follower.goToPoints(true, maxVel);
-                            break;
-                        }
-
-                    }
-
-                }
-                else if (blocks[i].id==2)
-                {
-                    pointToGoTo.add(new PosesAndActions(startPos, ""));
-                    pointToGoTo.add(new PosesAndActions(whitePos, ""));
-                    follower.init(pointToGoTo, false, true);
-                    waitForStart();
-                    follower.goToPoints(true, maxVel);
-                    pointToGoTo.clear();
-                    pointToGoTo.add(new PosesAndActions(startPos, ""));
-                    follower.reinit(pointToGoTo);
-                    follower.goToPoints(true, maxVel);
-                    break;
-                }
-                else if (blocks[i].id==1)
-                {
-                    pointToGoTo.add(new PosesAndActions(startPos, ""));
-                    pointToGoTo.add(new PosesAndActions(blue1Pos, ""));
-                    pointToGoTo.add(new PosesAndActions(blue2Pos, ""));
-                    pointToGoTo.add(new PosesAndActions(blue3Pos, ""));
-                    follower.init(pointToGoTo, false, true);
-                    waitForStart();
-                    follower.goToPoints(true, maxVel);
-                    pointToGoTo.clear();
-                    pointToGoTo.add(new PosesAndActions(startPos, ""));
-                    follower.reinit(pointToGoTo);
-                    follower.goToPoints(true, maxVel);
-                    break;
+            for (int k = 0; k < blocks.length; k++) {
+                int newArea = blocks[k].height * blocks[k].width;
+                if (newArea > largestArea) {
+                    largestArea = newArea;
+                    position = k;
                 }
             }
-
-            telemetry.update();
+            if (blocks[position].id == 1) {
+                telemetry.addLine("blue");
+                pointToGoTo.add(new PosesAndActions(startPos, ""));
+                pointToGoTo.add(new PosesAndActions(blue1Pos, ""));
+                pointToGoTo.add(new PosesAndActions(blue2Pos, ""));
+                pointToGoTo.add(new PosesAndActions(blue3Pos, ""));
+                follower.init(pointToGoTo, false, true);
+                waitForStart();
+                follower.goToPoints(true, maxVel);
+                pointToGoTo.clear();
+                pointToGoTo.add(new PosesAndActions(startPos, ""));
+                follower.reinit(pointToGoTo);
+                follower.goToPoints(true, maxVel);
+                break;
+            }
+            if (blocks[position].id == 2) {
+                telemetry.addLine("white");
+                pointToGoTo.add(new PosesAndActions(startPos, ""));
+                pointToGoTo.add(new PosesAndActions(whitePos, ""));
+                follower.init(pointToGoTo, false, true);
+                waitForStart();
+                follower.goToPoints(true, maxVel);
+                pointToGoTo.clear();
+                pointToGoTo.add(new PosesAndActions(startPos, ""));
+                follower.reinit(pointToGoTo);
+                follower.goToPoints(true, maxVel);
+                break;
+            }
+            if (blocks[position].id == 3) {
+                telemetry.addLine("red");
+                pointToGoTo.add(new PosesAndActions(startPos, ""));
+                pointToGoTo.add(new PosesAndActions(redPos, ""));
+                follower.init(pointToGoTo, false, true);
+                waitForStart();
+                follower.goToPoints(true, maxVel);
+                pointToGoTo.clear();
+                pointToGoTo.add(new PosesAndActions(startPos, ""));
+                follower.reinit(pointToGoTo);
+                follower.goToPoints(true, maxVel);
+                break;
+            }
         }
     }
 }
+            //blue = 1 white = 2 red = 3
+
