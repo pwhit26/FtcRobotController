f1e73be23121be4951913526d084279d7ec71615
pwhit26 <163192415+pwhit26@users.noreply.github.com>
2024-09-08 11:55:07 -0400

    first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/notAuton/ContinuousServo.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.notAuton;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class ContinuousServo extends LinearOpMode{
+    double forward = gamepad1.a
+}
